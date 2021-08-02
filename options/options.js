14597ab0c63c948279f8947d8a25aace11795d86
Crowdbotics <team@crowdbotics.com>
2021-08-02 21:24:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "shy_surf_29345Identifier",
+  url: "https://shy_surf_29345Identifier.botics.co",
+  api: "https://shy_surf_29345Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
